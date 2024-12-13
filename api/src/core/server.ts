b807d9059ac319459f1e03bd66230ff331cee8db
nehu3n <developer.nh3@gmail.com>
2024-12-13 01:17:12 -0300

    ✨ feat(api): Started Kito API by adding core functionality [WIP]

---
+++ api/src/core/server.ts
@@ -0,0 +1,45 @@
+import { KitoConfig, KitoInterface, Request, Response } from '../types/server'
+
+class Kito implements KitoInterface {
+	readonly config: KitoConfig
+
+	constructor(config?: KitoConfig) {
+		const DEFAULT_CONFIG: KitoConfig = {}
+
+		this.config = { ...DEFAULT_CONFIG, ...config }
+	}
+
+	listen(
+		options: { port: number; hostname?: string } | number,
+		callback?: () => void
+	): void {
+		const portConfig =
+			typeof options === 'number'
+				? { port: options, hostname: 'localhost' }
+				: { ...options, hostname: options.hostname || 'localhost' }
+
+		callback?.()
+	}
+
+	get(path: string, callback: (req: Request, res: Response) => void): void {}
+
+	post(path: string, callback: (req: Request, res: Response) => void): void {}
+
+	put(path: string, callback: (req: Request, res: Response) => void): void {}
+
+	patch(
+		path: string,
+		callback: (req: Request, res: Response) => void
+	): void {}
+
+	delete(
+		path: string,
+		callback: (req: Request, res: Response) => void
+	): void {}
+}
+
+function kito(options?: KitoConfig): Kito {
+	return new Kito(options)
+}
+
+export { kito }
