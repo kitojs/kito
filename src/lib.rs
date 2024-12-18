df0b38e4ece7167308071bc5ecfe421e7eb50fa2
nehu3n <developer.nh3@gmail.com>
2024-12-18 20:45:09 -0300

    🔨 refactor(core): Listening function implemented in a Server struct

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,31 @@ use std::ffi::{c_char, CStr};
 use actix_web::rt;
 use actix_web::{web, App, HttpResponse, HttpServer};
 
-fn listen(host: String, port: u16) {
-    let addr = format!("{}:{}", host, port);
-    rt::System::new().block_on(async {
-        HttpServer::new(|| {
-            App::new().service(
-                web::resource("/").to(|| async { HttpResponse::Ok().body("Hello world!") }),
-            )
-        })
-        .bind(&addr)
-        .expect("failed to bind address")
-        .run()
-        .await
-        .expect("server run failed");
-    });
+struct Server {
+    host: String,
+    port: u16,
+}
+
+impl Server {
+    fn new(host: String, port: u16) -> Self {
+        Server { host, port }
+    }
+
+    pub fn listen(&self) {
+        let addr = format!("{}:{}", self.host, self.port);
+        rt::System::new().block_on(async {
+            HttpServer::new(|| {
+                App::new().service(
+                    web::resource("/").to(|| async { HttpResponse::Ok().body("Hello world!") }),
+                )
+            })
+            .bind(&addr)
+            .expect("failed to bind address")
+            .run()
+            .await
+            .expect("server run failed");
+        });
+    }
 }
 
 #[no_mangle]
@@ -36,5 +47,6 @@ pub extern "C" fn run(host: *mut c_char, port: u16) {
         }
     };
 
-    listen(host_str, port);
+    let server = Server::new(host_str, port);
+    server.listen();
 }
