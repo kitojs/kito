6a7f856a7846e041b45cc91bf887141b28a38181
nehu3n <developer.nh3@gmail.com>
2025-09-08 20:27:19 -0300

    feat: add schema types

---
+++ packages/types/src/schema.ts
@@ -0,0 +1,45 @@
+export type SchemaBuilder<T> = { _output: T };
+
+export declare const t: {
+	str: () => SchemaBuilder<string> & {
+		min: (n: number) => SchemaBuilder<string>;
+		max: (n: number) => SchemaBuilder<string>;
+		regex: (r: RegExp) => SchemaBuilder<string>;
+		uuid: () => SchemaBuilder<string>;
+		email: () => SchemaBuilder<string>;
+		url: () => SchemaBuilder<string>;
+		default: (v: string) => SchemaBuilder<string>;
+	};
+
+	num: () => SchemaBuilder<number> & {
+		min: (n: number) => SchemaBuilder<number>;
+		max: (n: number) => SchemaBuilder<number>;
+		int: () => SchemaBuilder<number>;
+		positive: () => SchemaBuilder<number>;
+		negative: () => SchemaBuilder<number>;
+		default: (v: number) => SchemaBuilder<number>;
+	};
+
+	bool: () => SchemaBuilder<boolean> & {
+		default: (v: boolean) => SchemaBuilder<boolean>;
+	};
+
+	array: <T>(item: SchemaBuilder<T>) => SchemaBuilder<T[]> & {
+		min: (n: number) => SchemaBuilder<T[]>;
+		max: (n: number) => SchemaBuilder<T[]>;
+	};
+
+	object: <T extends Record<string, SchemaBuilder<any>>>(
+		shape: T,
+	) => SchemaBuilder<{
+		[K in keyof T]: T[K] extends SchemaBuilder<infer O> ? O : never;
+	}>;
+
+	union: <T extends SchemaBuilder<any>[]>(
+		alts: T,
+	) => SchemaBuilder<T[number] extends SchemaBuilder<infer O> ? O : never>;
+
+	literal: <T extends string | number | boolean>(v: T) => SchemaBuilder<T>;
+
+	optional: <T>(s: SchemaBuilder<T>) => SchemaBuilder<T | undefined>;
+};
