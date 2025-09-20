f5b0a3744a1a1d89f2e3ba6858b1067c3cf81054
nehu3n <developer.nh3@gmail.com>
2025-09-19 22:49:34 -0300

    feat(kitojs): add schemas exports

---
+++ packages/kitojs/src/schemas/index.ts
@@ -0,0 +1,10 @@
+// biome-ignore assist/source/organizeImports: ...
+export * from "./builders";
+
+export * from "./primitives/string";
+export * from "./primitives/number";
+export * from "./primitives/boolean";
+export * from "./primitives/array";
+export * from "./primitives/object";
+export * from "./primitives/literal";
+export * from "./primitives/union";
