682a582ca67541a33debd4badcf40eaab9750704
nehu3n <developer.nh3@gmail.com>
2025-09-20 20:19:40 -0300

    feat(cli): add clap

--- packages/cli/src/main.rs
+++ packages/cli/src/main.rs
@@ -1,3 +1,20 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(
+    name = "kito",
+    version = env!("CARGO_PKG_VERSION"),
+    about = env!("CARGO_PKG_DESCRIPTION")
+)]
+#[command(arg_required_else_help = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {}
+
 fn main() {
-    println!("hello world!");
+    println!("Hello, world!");
 }
