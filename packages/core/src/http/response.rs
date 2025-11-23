e1c4c7ea1c8156355fe68f2eca1daaa712d0b19c
nehu3n <developer.nh3@gmail.com>
2025-11-22 22:24:13 -0300

    feat(core): add streaming response support

--- packages/core/src/http/response.rs
+++ packages/core/src/http/response.rs
@@ -1,9 +1,5 @@
-use http_body_util::Full;
-use hyper::{
-    Response,
-    body::Bytes,
-    header::{HeaderName, HeaderValue},
-};
+use http_body_util::{Full, StreamBody, combinators::BoxBody};
+use hyper::body::{Bytes, Frame};
 
 use napi::bindgen_prelude::{Buffer, External};
 
@@ -11,17 +7,31 @@ use parking_lot::Mutex;
 use serde_json::from_slice;
 
 use std::{collections::HashMap, sync::Arc};
-use tokio::sync::oneshot::Sender;
+use tokio::sync::mpsc::UnboundedSender;
 
 pub struct ResponseChannel {
-    pub tx: Mutex<Option<Sender<Response<Full<Bytes>>>>>,
+    pub tx: Mutex<Option<UnboundedSender<ResponseMessage>>>,
 }
 
 impl ResponseChannel {
-    pub fn new(tx: Sender<Response<Full<Bytes>>>) -> Self {
+    pub fn new(tx: UnboundedSender<ResponseMessage>) -> Self {
         Self { tx: Mutex::new(Some(tx)) }
     }
 }
+
+pub enum ResponseMessage {
+    Complete { status: u16, headers: Vec<(String, String)>, body: Bytes },
+    StreamStart { status: u16, headers: Vec<(String, String)> },
+    StreamChunk { data: Bytes },
+    StreamEnd,
+}
+
+pub enum ResponseBody {
+    Full(Full<Bytes>),
+    Stream(StreamBody<Frame<Bytes>>),
+}
+
+pub type BoxedBody = BoxBody<Bytes, Box<dyn std::error::Error + Send + Sync>>;
 
 /// buffer: [status_code(2)] [headers_len(4)] [headers_json] [body]
 #[napi]
@@ -51,26 +61,65 @@ pub fn send_response(channel: &External<Arc<ResponseChannel>>, buffer: Buffer) -
         Bytes::new()
     };
 
-    let mut response = Response::builder().status(status_code);
-
-    for (name, value) in headers {
-        if let (Ok(header_name), Ok(header_value)) =
-            (HeaderName::try_from(name.as_str()), HeaderValue::try_from(value.as_str()))
-        {
-            response = response.header(header_name, header_value);
-        }
-    }
-
-    let response = response
-        .body(Full::new(body))
-        .map_err(|e| napi::Error::from_reason(format!("Failed to build response: {e}")))?;
-
     let mut tx_guard = channel.tx.lock();
-    if let Some(tx) = tx_guard.take() {
-        let _ = tx.send(response);
+    if let Some(tx) = tx_guard.as_ref() {
+        let _ = tx.send(ResponseMessage::Complete { status: status_code, headers, body });
+        *tx_guard = None;
         Ok(())
     } else {
         Err(napi::Error::from_reason("Response already sent"))
+    }
+}
+
+/// buffer: [status_code(2)] [headers_len(4)] [headers_json]
+#[napi]
+pub fn start_stream(channel: &External<Arc<ResponseChannel>>, buffer: Buffer) -> napi::Result<()> {
+    let data = buffer.as_ref();
+
+    if data.len() < 6 {
+        return Err(napi::Error::from_reason("Invalid stream start buffer"));
+    }
+
+    let status_code = u16::from_le_bytes([data[0], data[1]]);
+    let headers_len = u32::from_le_bytes([data[2], data[3], data[4], data[5]]) as usize;
+
+    if data.len() < 6 + headers_len {
+        return Err(napi::Error::from_reason("Invalid headers length"));
+    }
+
+    let headers_json = &data[6..6 + headers_len];
+    let headers: Vec<(String, String)> = from_slice(headers_json)
+        .map_err(|e| napi::Error::from_reason(format!("Invalid headers JSON: {e}")))?;
+
+    let tx_guard = channel.tx.lock();
+    if let Some(tx) = tx_guard.as_ref() {
+        let _ = tx.send(ResponseMessage::StreamStart { status: status_code, headers });
+        Ok(())
+    } else {
+        Err(napi::Error::from_reason("Response already sent"))
+    }
+}
+
+#[napi]
+pub fn send_chunk(channel: &External<Arc<ResponseChannel>>, data: Buffer) -> napi::Result<()> {
+    let tx_guard = channel.tx.lock();
+    if let Some(tx) = tx_guard.as_ref() {
+        let bytes = Bytes::copy_from_slice(data.as_ref());
+        let _ = tx.send(ResponseMessage::StreamChunk { data: bytes });
+        Ok(())
+    } else {
+        Err(napi::Error::from_reason("Stream not started"))
+    }
+}
+
+#[napi]
+pub fn end_stream(channel: &External<Arc<ResponseChannel>>) -> napi::Result<()> {
+    let mut tx_guard = channel.tx.lock();
+    if let Some(tx) = tx_guard.take() {
+        let _ = tx.send(ResponseMessage::StreamEnd);
+        Ok(())
+    } else {
+        Err(napi::Error::from_reason("Stream already ended"))
     }
 }
 
