5eaaaa9bbfdbdf841763428df5f1423c4561e7f3
nehu3n <developer.nh3@gmail.com>
2025-12-06 19:31:23 -0300

    refactor(types): simplify KitoServerInstance by extending KitoRouterInstance

--- packages/types/src/server.d.ts
+++ packages/types/src/server.d.ts
@@ -1,6 +1,7 @@
 import type { MiddlewareHandler, RouteHandler } from "./handlers";
 import type { MiddlewareDefinition, RouteChain } from "./routes";
 import type { SchemaDefinition } from "./schema/base";
+import type { KitoRouterInstance } from "./router";
 
 export interface ServerOptions {
   port?: number;
@@ -13,97 +14,8 @@ export interface ServerOptions {
 }
 
 // biome-ignore lint/complexity/noBannedTypes: ...
-export interface KitoServerInstance<TExtensions = {}> {
-  use(
-    middleware: MiddlewareDefinition | MiddlewareHandler,
-  ): KitoServerInstance<TExtensions>;
-
-  // biome-ignore lint/complexity/noBannedTypes: ...
-  get<TSchema extends SchemaDefinition = {}>(
-    path: string,
-    handler: RouteHandler<TSchema, TExtensions>,
-  ): KitoServerInstance<TExtensions>;
-  // biome-ignore lint/complexity/noBannedTypes: ...
-  get<TSchema extends SchemaDefinition = {}>(
-    path: string,
-    middlewares: (MiddlewareDefinition | TSchema)[],
-    handler: RouteHandler<TSchema, TExtensions>,
-  ): KitoServerInstance<TExtensions>;
-
-  // biome-ignore lint/complexity/noBannedTypes: ...
-  post<TSchema extends SchemaDefinition = {}>(
-    path: string,
-    handler: RouteHandler<TSchema, TExtensions>,
-  ): KitoServerInstance<TExtensions>;
-  // biome-ignore lint/complexity/noBannedTypes: ...
-  post<TSchema extends SchemaDefinition = {}>(
-    path: string,
-    middlewares: (MiddlewareDefinition | TSchema)[],
-    handler: RouteHandler<TSchema, TExtensions>,
-  ): KitoServerInstance<TExtensions>;
-
-  // biome-ignore lint/complexity/noBannedTypes: ...
-  put<TSchema extends SchemaDefinition = {}>(
-    path: string,
-    handler: RouteHandler<TSchema, TExtensions>,
-  ): KitoServerInstance<TExtensions>;
-  // biome-ignore lint/complexity/noBannedTypes: ...
-  put<TSchema extends SchemaDefinition = {}>(
-    path: string,
-    middlewares: (MiddlewareDefinition | TSchema)[],
-    handler: RouteHandler<TSchema, TExtensions>,
-  ): KitoServerInstance<TExtensions>;
-
-  // biome-ignore lint/complexity/noBannedTypes: ...
-  delete<TSchema extends SchemaDefinition = {}>(
-    path: string,
-    handler: RouteHandler<TSchema, TExtensions>,
-  ): KitoServerInstance<TExtensions>;
-  // biome-ignore lint/complexity/noBannedTypes: ...
-  delete<TSchema extends SchemaDefinition = {}>(
-    path: string,
-    middlewares: (MiddlewareDefinition | TSchema)[],
-    handler: RouteHandler<TSchema, TExtensions>,
-  ): KitoServerInstance<TExtensions>;
-
-  // biome-ignore lint/complexity/noBannedTypes: ...
-  patch<TSchema extends SchemaDefinition = {}>(
-    path: string,
-    handler: RouteHandler<TSchema, TExtensions>,
-  ): KitoServerInstance<TExtensions>;
-  // biome-ignore lint/complexity/noBannedTypes: ...
-  patch<TSchema extends SchemaDefinition = {}>(
-    path: string,
-    middlewares: (MiddlewareDefinition | TSchema)[],
-    handler: RouteHandler<TSchema, TExtensions>,
-  ): KitoServerInstance<TExtensions>;
-
-  // biome-ignore lint/complexity/noBannedTypes: ...
-  head<TSchema extends SchemaDefinition = {}>(
-    path: string,
-    handler: RouteHandler<TSchema, TExtensions>,
-  ): KitoServerInstance<TExtensions>;
-  // biome-ignore lint/complexity/noBannedTypes: ...
-  head<TSchema extends SchemaDefinition = {}>(
-    path: string,
-    middlewares: (MiddlewareDefinition | TSchema)[],
-    handler: RouteHandler<TSchema, TExtensions>,
-  ): KitoServerInstance<TExtensions>;
-
-  // biome-ignore lint/complexity/noBannedTypes: ...
-  options<TSchema extends SchemaDefinition = {}>(
-    path: string,
-    handler: RouteHandler<TSchema, TExtensions>,
-  ): KitoServerInstance<TExtensions>;
-  // biome-ignore lint/complexity/noBannedTypes: ...
-  options<TSchema extends SchemaDefinition = {}>(
-    path: string,
-    middlewares: (MiddlewareDefinition | TSchema)[],
-    handler: RouteHandler<TSchema, TExtensions>,
-  ): KitoServerInstance<TExtensions>;
-
-  route(path: string): RouteChain<TExtensions>;
-
+export interface KitoServerInstance<TExtensions = {}>
+  extends KitoRouterInstance<TExtensions> {
   listen(callback?: () => void): Promise<ServerOptions>;
   listen(port?: number, callback?: () => void): Promise<ServerOptions>;
   listen(
