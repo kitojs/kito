0b18ba8477e3f31dd62d951d5f58625cf9ec416e
nehu3n <developer.nh3@gmail.com>
2025-12-06 19:25:56 -0300

    feat(types): add router types

---
+++ packages/types/src/router.d.ts
@@ -0,0 +1,101 @@
+import type { MiddlewareHandler, RouteHandler } from "./handlers";
+import type { MiddlewareDefinition, RouteChain } from "./routes";
+import type { SchemaDefinition } from "./schema/base";
+
+// biome-ignore lint/complexity/noBannedTypes: ...
+export interface KitoRouterInstance<TExtensions = {}> {
+  use(
+    middleware: MiddlewareDefinition | MiddlewareHandler,
+  ): KitoRouterInstance<TExtensions>;
+
+  mount(
+    path: string,
+    router: KitoRouterInstance<TExtensions>,
+  ): KitoRouterInstance<TExtensions>;
+
+  // biome-ignore lint/complexity/noBannedTypes: ...
+  get<TSchema extends SchemaDefinition = {}>(
+    path: string,
+    handler: RouteHandler<TSchema, TExtensions>,
+  ): KitoRouterInstance<TExtensions>;
+  // biome-ignore lint/complexity/noBannedTypes: ...
+  get<TSchema extends SchemaDefinition = {}>(
+    path: string,
+    middlewares: (MiddlewareDefinition | TSchema)[],
+    handler: RouteHandler<TSchema, TExtensions>,
+  ): KitoRouterInstance<TExtensions>;
+
+  // biome-ignore lint/complexity/noBannedTypes: ...
+  post<TSchema extends SchemaDefinition = {}>(
+    path: string,
+    handler: RouteHandler<TSchema, TExtensions>,
+  ): KitoRouterInstance<TExtensions>;
+  // biome-ignore lint/complexity/noBannedTypes: ...
+  post<TSchema extends SchemaDefinition = {}>(
+    path: string,
+    middlewares: (MiddlewareDefinition | TSchema)[],
+    handler: RouteHandler<TSchema, TExtensions>,
+  ): KitoRouterInstance<TExtensions>;
+
+  // biome-ignore lint/complexity/noBannedTypes: ...
+  put<TSchema extends SchemaDefinition = {}>(
+    path: string,
+    handler: RouteHandler<TSchema, TExtensions>,
+  ): KitoRouterInstance<TExtensions>;
+  // biome-ignore lint/complexity/noBannedTypes: ...
+  put<TSchema extends SchemaDefinition = {}>(
+    path: string,
+    middlewares: (MiddlewareDefinition | TSchema)[],
+    handler: RouteHandler<TSchema, TExtensions>,
+  ): KitoRouterInstance<TExtensions>;
+
+  // biome-ignore lint/complexity/noBannedTypes: ...
+  delete<TSchema extends SchemaDefinition = {}>(
+    path: string,
+    handler: RouteHandler<TSchema, TExtensions>,
+  ): KitoRouterInstance<TExtensions>;
+  // biome-ignore lint/complexity/noBannedTypes: ...
+  delete<TSchema extends SchemaDefinition = {}>(
+    path: string,
+    middlewares: (MiddlewareDefinition | TSchema)[],
+    handler: RouteHandler<TSchema, TExtensions>,
+  ): KitoRouterInstance<TExtensions>;
+
+  // biome-ignore lint/complexity/noBannedTypes: ...
+  patch<TSchema extends SchemaDefinition = {}>(
+    path: string,
+    handler: RouteHandler<TSchema, TExtensions>,
+  ): KitoRouterInstance<TExtensions>;
+  // biome-ignore lint/complexity/noBannedTypes: ...
+  patch<TSchema extends SchemaDefinition = {}>(
+    path: string,
+    middlewares: (MiddlewareDefinition | TSchema)[],
+    handler: RouteHandler<TSchema, TExtensions>,
+  ): KitoRouterInstance<TExtensions>;
+
+  // biome-ignore lint/complexity/noBannedTypes: ...
+  head<TSchema extends SchemaDefinition = {}>(
+    path: string,
+    handler: RouteHandler<TSchema, TExtensions>,
+  ): KitoRouterInstance<TExtensions>;
+  // biome-ignore lint/complexity/noBannedTypes: ...
+  head<TSchema extends SchemaDefinition = {}>(
+    path: string,
+    middlewares: (MiddlewareDefinition | TSchema)[],
+    handler: RouteHandler<TSchema, TExtensions>,
+  ): KitoRouterInstance<TExtensions>;
+
+  // biome-ignore lint/complexity/noBannedTypes: ...
+  options<TSchema extends SchemaDefinition = {}>(
+    path: string,
+    handler: RouteHandler<TSchema, TExtensions>,
+  ): KitoRouterInstance<TExtensions>;
+  // biome-ignore lint/complexity/noBannedTypes: ...
+  options<TSchema extends SchemaDefinition = {}>(
+    path: string,
+    middlewares: (MiddlewareDefinition | TSchema)[],
+    handler: RouteHandler<TSchema, TExtensions>,
+  ): KitoRouterInstance<TExtensions>;
+
+  route(path: string): RouteChain<TExtensions>;
+}
